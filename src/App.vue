c60e82266c8308a69c60a02773922108c26e8cb8
Noah Mulfinger <noah.mulfinger@gmail.com>
2018-01-25 14:45:05 -0800

    Remove components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="wrapper">
-    <MiniMap class="minimap"/>
-    <MainMap class="mainmap"/>
+    <div class="side-bar-container">
+      <div class="header panel-dark">
+        <h1 class="result-title trailer-0">Results</h1>
+      </div>
+      <ul>
+        <li v-for="(module, index) in modules" :key="index">
+          {{ module }}
+        </li>
+      </ul>
+    </div>
+    <div class="map-container">
+      <div id="mapView">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,22 +26,66 @@
   flex-flow: row;
 }
 
-.mainmap {
+.side-bar-container {
+  flex: 1 50%;
+}
+
+.map-container {
   flex: 1 100%;
 }
 
-.minimap {
-  flex: 1 50%;
+#mapView {
+  height: 100%;
+  width: 100%;
+}
+
+.header {
+  text-align: left;
+  padding: 10px;
 }
 </style>
 
 <script>
-import MainMap from "./components/MainMap.vue";
-import MiniMap from "./components/MiniMap.vue";
+import { loadModules } from "esri-loader";
 export default {
-  components: {
-    MainMap,
-    MiniMap
+  data () {
+    return {
+      modules: ["esri/Map", "esri/views/MapView", "esri/layers/TileLayer", "esri/identity/IdentityManager"],
+
+    };
+  },
+  mounted () {
+    loadModules(this.modules).then(this.createMap);
+  },
+  methods: {
+    createMap ([Map, MapView, TileLayer, IdentityManager]) {
+      IdentityManager.registerToken({
+        token: "hJwledWh1hzk0Ywy6QDzgmVTyszRJfu0Gl9EAY2DFGcUlHr13PAsi3i1GxSbE4W6bNT9trKPJuo008iTaNLeq2gJs2xIU5Kaiw4dX4vMEjpMHeIxtPr1PpcSKu6PNriYQwrAdrOK1DHrx4N0feQp3A..",
+        server: "https://www.arcgis.com/sharing/rest"
+      })
+      const layer = new TileLayer({
+        url: "https://tiledbasemaps.arcgis.com/arcgis/rest/services/World_Imagery/MapServer"
+      });
+      const map = new Map({
+        layers: [layer],
+      });
+
+
+      const view = new MapView({
+        map: map,
+        container: "mapView",
+        center: [-117.19567090269645, 34.057266067316206],
+        zoom: 16
+      });
+
+      // view.on("click", event => {
+      //   console.log(event.mapPoint);
+      // });
+
+      // view.when(() => {
+      //   console.log(layer);
+      // })
+    }
   }
 }
 </script>
