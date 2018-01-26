560b8f244a7a4c1a2779fbd68ce40004f529d64c
Aysar Khalid <ak@akpro.net>
2018-01-25 17:33:23 -0800

    Minor styling changes

--- src/app/app.vue
+++ src/app/app.vue
@@ -2,29 +2,25 @@
   <div class="wrapper">
     <div class="side-bar-container">
       <div class="header">
-        <div v-on:click="switchMode('results')" class="results-header panel panel-dark panel-no-border">
-          <h1 class="result-title trailer-0">Results</h1>
-        </div>
-        <div v-on:click="switchMode('train')" class="train-header panel panel-no-border">
-          <h1 class="result-title trailer-0">Train</h1>
-        </div>
+        <nav class="leader-1">
+          <a href="#" class="btn btn-grouped" v-on:click="switchMode('results')">SEARCH</a>
+          <a href="#" class="btn btn-grouped" v-on:click="switchMode('train')">TRAIN</a>
+        </nav>
       </div>
       <div v-if="mode === 'results'">
-      <ul>
-        <li v-for="(m, index) in modules" :key="index">
-          {{ m }}
-        </li>
-      </ul>
+        <ul>
+          <li v-for="(m, index) in modules" :key="index">
+            {{ m }}
+          </li>
+        </ul>
       </div>
       <div v-else>
         Training mode goes here
         <div>
           <input type="checkbox" id="ml_flag" name="ml_flag">
           <label for="ml_flag">ML flag</label>
+        </div>
       </div>
-      </div>
-
-
     </div>
     <div class="map-container">
       <div id="mapView">
